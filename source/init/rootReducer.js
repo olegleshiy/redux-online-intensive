e60a1c8a35ccfddf3862159d02afad38d7b886da
olegleshiy <olegleshiy7@gmail.com>
2019-08-26 02:36:51 +0300

    add homework-4

--- source/init/rootReducer.js
+++ source/init/rootReducer.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 
 //Reducers
 import { postsReducer as posts } from '../bus/posts/reducer';
-import { uiReducer as ui } from "../bus/ui/reducer";
+import { uiReducer as ui } from '../bus/ui/reducer';
 
 export const rootReducer = combineReducers({
     posts,
